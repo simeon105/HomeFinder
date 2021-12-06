e2c3df213bb20a685793c168195cccb235ce337c
evaagovska <evaagovska@gmail.com>
2021-12-07 00:05:01 +0100

    domasna2

---
+++ domasna2/tech prototype/src/main/java/com/example/yourlocalpharmacy/YourLocalPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.yourlocalpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourLocalPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourLocalPharmacyApplication.class, args);
+    }
+
+}
